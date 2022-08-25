b397cc42702945e974ba721a3eaa863d4f09e02d
Documenter.jl <documenter@juliadocs.github.io>
2022-08-25 10:35:05 +0000

    build based on 98ef2ee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RiverREM","category":"page"},{"location":"#RiverREM","page":"Home","title":"RiverREM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RiverREM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RiverREM]","category":"page"}]
+}
